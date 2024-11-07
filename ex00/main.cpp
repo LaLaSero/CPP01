65606cac19d75b94e367da3d67b5fb5265460db1
LaLaSero <filia2444@gmsil.com>
2024-11-07 16:41:56 +0900

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,6 +5,11 @@ int main()
 	Zombie *zombie;
 	
 	zombie = newZombie("New Zombie");
+	if (!zombie)
+	{
+		std::cout << "Memory allocation failed" << std::endl;
+		return (1);
+	}
 	zombie->announce();
 	randomChump("Random Zombie");
 	delete zombie;
--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -3,5 +3,7 @@
 Zombie *newZombie(std::string name)
 {
 	Zombie *NewZombie = new Zombie(name);
+	if (!NewZombie)
+		return (NULL);
 	return (NewZombie);
 }
